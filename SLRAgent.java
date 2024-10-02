79df79caa543de0ffd53990818896060afba4f88
ninoxander <za220111414@zapopan.tecmm.edu.mx>
2024-10-01 19:18:12 -0600

    :construction: Added the main file of this agent based project: SLRAgent.java

---
+++ SLRAgent.java
@@ -0,0 +1,10 @@
+import jade.core.Agent;
+import src.DiscreteMath;
+import src.DataSet;
+
+public class SLRAgent extends Agent{
+    @Override
+    protected void setup() {
+        System.out.println("Hello world!");
+    }
+}
