be9ebed0597dee18dd2beb95cbde498fa08a050a
ninoxander <za220111414@zapopan.tecmm.edu.mx>
2024-10-01 21:14:44 -0600

    :construction: Code was refactored to simplify functionality. New features were added on discrete maths class

--- SLRAgent.java
+++ SLRAgent.java
@@ -1,10 +1,41 @@
 import jade.core.Agent;
+import jade.core.behaviours.OneShotBehaviour;
 import src.DiscreteMath;
 import src.DataSet;
 
 public class SLRAgent extends Agent{
     @Override
     protected void setup() {
-        System.out.println("Hello world!");
+        // Create a new dataset
+        DataSet dataset = new DataSet();
+
+        System.out.println("I'm " + getLocalName() +  " and I'm running!");
+
+        // Starting behaviour
+        addBehaviour(new SLRBehaviour(dataset));
+    }
+
+    private class SLRBehaviour extends OneShotBehaviour {
+        private DiscreteMath maths;
+        private DataSet dataset;
+        public SLRBehaviour(DataSet ds){
+            this.dataset = ds;
+            this.maths = new DiscreteMath(this.dataset);
+        }
+
+        @Override
+        public void action(){
+            // Printing data
+            System.out.println("SLR Algorithm by Daniela Nava");
+            this.maths.linearRegression();
+
+            System.out.println("Beta 0 = " + this.maths.getBeta0());
+            System.out.println("Beta 1 = " + this.maths.getBeta1());
+            System.out.println("Beta 0 SE = " +  this.maths.B0StandError);
+            System.out.println("Beta 1 SE = " + this.maths.B1StandError);
+            System.out.println("R Squared = " + this.maths.RSquared);
+            System.out.println("R correlation = " + this.maths.R);
+
+        }
     }
 }
--- src/DiscreteMath.java
+++ src/DiscreteMath.java
@@ -1,78 +1,164 @@
 package src;
 
 public class DiscreteMath {
-    DataSet dataSet = new DataSet();
-    public DiscreteMath(DataSet _dataSet){
+    private DataSet dataSet;
+    public float QuadT, t, varB0, varB1;
+    public float B0StandError, B1StandError;
+    public float SRC, SEC, STC;
+    public float RSquared, R;
+    public float MCX;
+
+    // Constructor
+    public DiscreteMath(DataSet _dataSet) {
         this.dataSet = _dataSet;
     }
-    // Calcula la sumatoria de los items de x.
-    public float sumatoriaX(){
-        int n = this.dataSet.getSizeX();
-        float[] x = dataSet.getX();
-        float resultadoSumatoria = 0;
-        for (int i = 0; i < n; i++){
-            // System.out.println("data x: " + x[i]);
-            resultadoSumatoria+= x[i];
+
+    // Function to get the sum of given array items
+    public float getSum(float[] data) {
+        float sum = 0;
+        for (float v : data) {
+            sum += v;
         }
-        return resultadoSumatoria;
+        return sum;
     }
-    // Calcula la sumtaoria de los items de y.
-    public float sumatoriaY(){
-        float[] y = this.dataSet.getY();
-        int n = this.dataSet.getSizeY();
-        float resutadoSumatoria = 0;
-        for (int i = 0; i < n; i++){
-            resutadoSumatoria+= y[i];
-        }
-        return resutadoSumatoria;
+
+    // Get x sum
+    public float getXSum() {
+        return getSum(dataSet.getX());
     }
-    // Calcula la mediana de X
-    public float medianaX(){
-        return this.sumatoriaX() / this.dataSet.getSizeX();
+
+    // Get y sum
+    public float getYSum() {
+        return getSum(dataSet.getY());
     }
-    // Calcula la mediana de Y
-    public float medianaY() {
-        return this.sumatoriaY() / this.dataSet.getSizeY();
+
+    // Get average of x
+    public float getMaX() {
+        return getXSum() / dataSet.getSizeX();
     }
-    // Calcula el cociente de xi * yi
-    public float[] multXY(){
-        float[] mult = new float[this.dataSet.getSizeX()];
-        float[] dataX = this.dataSet.getX();
-        float[] dataY = this.dataSet.getY();
-        for(int i = 0; i < this.dataSet.getSizeX(); i++){
+
+    // Get average of y
+    public float getMaY() {
+        return getYSum() / dataSet.getSizeY();
+    }
+
+    // Get xi (original x values)
+    public float[] getXi() {
+        return dataSet.getX(); // Simplemente devolver los valores originales
+    }
+
+    // Get yi (original y values)
+    public float[] getYi() {
+        return dataSet.getY(); // Simplemente devolver los valores originales
+    }
+
+    // Get xi * yi
+    public float[] getXI() {
+        float[] dataX = dataSet.getX();
+        float[] dataY = dataSet.getY();
+        float[] mult = new float[dataSet.getSizeX()];
+
+        for (int i = 0; i < dataSet.getSizeX(); i++) {
             mult[i] = dataY[i] * dataX[i];
-            //System.out.println(datay[i]+" "+dataX[i]);
         }
-        return  mult;
+        return mult;
     }
-    // Calcula la sumtaoria de los cocientes xi * yi
-    public float sumatoriaXY(){
-        float sumatoria = 0;
-        float[] mult = this.multXY();
-        for(int i = 0; i < mult.length ;i++){
-            sumatoria+= mult[i];
+
+    // Get xi * yi sum
+    public float getXYSum() {
+        return getSum(getXI());
+    }
+
+    // Get quad x
+    public float[] getQuadX() {
+        float[] quadx = new float[dataSet.getSizeX()];
+        float[] x = dataSet.getX();
+        for (int i = 0; i < dataSet.getSizeX(); i++) {
+            quadx[i] = x[i] * x[i];
         }
-        return sumatoria;
+        return quadx;
     }
-    // Calcula la sumtaoria cuadrada de cada item en x
-    public float sumatoriaXCuadrada() {
-        float sumatoria = 0;
-        float[] xcuadrada = this.dataSet.getX();
-        for(int i = 0; i < this.dataSet.getSizeX(); i++){
-            sumatoria += (float) Math.pow((double) xcuadrada[i], 2);
-        }
-        return sumatoria;
+
+    // Get quad x sum
+    public float getQuadXSum() {
+        return getSum(getQuadX());
     }
-    // Calcula beta 1
-    float beta1() {
-        float num = this.dataSet.getSizeX() * sumatoriaXY() - (sumatoriaX() * sumatoriaY());
-        float den = this.dataSet.getSizeX() * sumatoriaXCuadrada() - (sumatoriaX() * sumatoriaX());
+
+    // Get beta 1
+    public float getBeta1() {
+        float num = this.dataSet.getSizeX() * getXYSum() - (getXSum() * getYSum());
+        float den = this.dataSet.getSizeX() * getQuadXSum() - (getXSum() * getXSum());
         return num / den;
     }
-    // Calcula beta 2
-    float beta0() {
-        float num = (sumatoriaXCuadrada() * sumatoriaY()) - (sumatoriaX() * sumatoriaXY());
-        float den = (this.dataSet.getSizeX() * sumatoriaXCuadrada()) - (float) (Math.pow(sumatoriaX(), 2));
-        return num / den;
+
+    // Get beta 0
+    public float getBeta0() {
+        return getMaY() - getBeta1() * getMaX();
+    }
+
+    // Get y sample
+    public float[] getYBar() {
+        float[] ysample = new float[this.dataSet.getSizeX()];
+        float[] xi = this.getXi();
+        float beta0 = getBeta0();
+        float beta1 = getBeta1();
+
+        for (int i = 0; i < this.dataSet.getSizeX(); i++) {
+            ysample[i] = beta0 + beta1 * xi[i]; // Usar los valores originales de xi
+        }
+        return ysample;
+    }
+
+    // Get ui
+    public float[] getUI() {
+        float[] ui = new float[dataSet.getSizeX()];
+        float[] ym = getYBar();
+        float[] y = dataSet.getY();
+
+        for (int i = 0; i < dataSet.getSizeX(); i++) {
+            ui[i] = y[i] - ym[i];
+        }
+        return ui;
+    }
+
+    // Get quad ui
+    public float[] getQuadUI() {
+        float[] ui = getUI();
+        float[] quadUI = new float[dataSet.getSizeX()];
+        for (int i = 0; i < dataSet.getSizeX(); i++) {
+            quadUI[i] = ui[i] * ui[i];
+        }
+        return quadUI;
+    }
+
+    // Linear Regression
+    public void linearRegression() {
+        // Sums
+        int size = this.dataSet.getSizeX();
+        float sumxi = this.getSum(this.getXI());
+        float sumyi = this.getSum(this.getYi());
+        float sumx2 = this.getSum(this.getQuadX());
+        float sumym = this.getSum(this.getYBar());
+        float sumui = this.getSum(this.getUI());
+        float sumqu = this.getSum(this.getQuadUI());
+
+        // Calculate coefficients
+        this.QuadT = sumqu / (size - 2);
+        this.t = (float) Math.sqrt((double) this.QuadT);
+
+        // Coefficients
+        float beta1 = getBeta1();
+        float beta0 = getBeta0();
+
+        this.B1StandError = (float) Math.sqrt((this.QuadT / (size - 2)) / (this.getQuadXSum() - ((getXSum() * getXSum()) / size)));
+        this.B0StandError = (float) Math.sqrt((this.QuadT / (size - 2)) * (1.0 / size + (getMaX() * getMaX()) / (this.getQuadXSum() - ((getXSum() * getXSum()) / size))));
+
+        this.SRC = sumqu;
+        // System.out.println("!!!!!!!!! debug"  + this.SRC);
+        this.SEC = (beta1 * beta1) * sumx2;
+        this.STC = this.SEC + this.SRC;
+        this.RSquared = 1 - (this.SRC / this.STC);
+        this.R = (float) Math.sqrt((double) this.RSquared);
+        this.MCX = (float) Math.sqrt((double) ((sumx2 - this.getQuadXSum()) / size));
     }
 }
